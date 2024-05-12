16fcc5b10d2cf9092199b6648d1abf2f539b0b36
leandro <lgomesb@gmail.com>
2024-05-12 22:06:34 +0100

    Normalize tests

--- src/test/java/com/barbosa/ms/invetorymgmt/productorder/controllers/ProductOrderControllerTest.java
+++ src/test/java/com/barbosa/ms/invetorymgmt/productorder/controllers/ProductOrderControllerTest.java
@@ -2,9 +2,13 @@ package com.barbosa.ms.invetorymgmt.productorder.controllers;
 
 import com.barbosa.ms.invetorymgmt.productorder.ProductOrderApplicationTests;
 import com.barbosa.ms.invetorymgmt.productorder.controller.ProductOrderController;
+import com.barbosa.ms.invetorymgmt.productorder.domain.dto.OrderItemDTO;
+import com.barbosa.ms.invetorymgmt.productorder.domain.dto.ProductOrderRequestDTO;
 import com.barbosa.ms.invetorymgmt.productorder.domain.records.OrderItemRecord;
 import com.barbosa.ms.invetorymgmt.productorder.domain.records.in.ProductOrderRecordIn;
 import com.barbosa.ms.invetorymgmt.productorder.services.ProductOrderService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.*;
@@ -20,15 +24,14 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -38,7 +41,7 @@ import static org.mockito.Mockito.when;
 @TestInstance(Lifecycle.PER_CLASS)
 class ProductOrderControllerTest {
 
-    private static UUID STATIC_UUID;
+    private static UUID PRODUCT_ORDER_UUID = UUID.randomUUID();
 
     @Value("${server.servlet.context-path}/")
     private String STATIC_URI;
@@ -54,10 +57,15 @@ class ProductOrderControllerTest {
 
     private ProductOrderRecordIn productorderRecordIn;
 
+    private final Given given = new Given();
+    private final When when = new When();
+    private final Then then = new Then();
+
+
     @BeforeEach
     void setup() {
         productorderRecordIn = ProductOrderRecordIn.builder()
-                .id(UUID.randomUUID())
+                .id(PRODUCT_ORDER_UUID)
                 .status("A")
                 .description("test-01")
                 .items(Collections.singleton(new OrderItemRecord(UUID.randomUUID(), 1)))
@@ -66,112 +74,176 @@ class ProductOrderControllerTest {
     }
 
     @Test
-    @Order(0)
-    void shouldSucceededWhenCallCreate() throws UnknownHostException {
-
-        when(service.create(any(ProductOrderRecordIn.class))).thenReturn(productorderRecordIn);
-
-        Response response = given()
-            .port(port)
-            .contentType(ContentType.JSON)
-            .body("{\"description\": \"string\",\"items\": [{\"productId\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\",\"quantity\": 1}]}")
-            .log().all()
-            .when()
-            .post(STATIC_URI)
-            .then()
-            .assertThat()
-            .statusCode(HttpStatus.CREATED.value())
-            .extract()
-            .response();
-            
-        assertNotNull(response);
-        String idProductOrder = response.getHeader("Location");
-        idProductOrder = idProductOrder.substring(idProductOrder.lastIndexOf("/")+1);
-        assertNotNull(idProductOrder);
-        assertFalse(idProductOrder.isEmpty());
-        STATIC_UUID = UUID.fromString(idProductOrder);
-
+    void shouldSucceededWhenCallCreate() throws Exception {
+        String productOrderRequestJSON = given.productOrderRequestJSON();
+        given.productOrderCreationService();
+        Response response = when.postProductOrder(productOrderRequestJSON);
+        then.productOrderCreationResponseIsValid(response);
     }
 
     @Test
-    @Order(1)
     void shouldSucceededWhenCallFindById() {
-        when(service.findById(any(UUID.class))).thenReturn(productorderRecordIn);
-
-        Response response = given()
-            .port(port)
-            .contentType(ContentType.JSON)
-            .pathParam("id", STATIC_UUID.toString())
-            .when()
-            .get(STATIC_URI + "{id}")
-            .then()
-            .log().all()
-            .assertThat()
-            .statusCode(HttpStatus.OK.value())
-            .extract()
-            .response();
-
-        assertNotNull(response);
-        assertNotNull(response.getBody().jsonPath().getString("id"));
-
+        given.productOrderSearched();
+        Response response = when.findProductOrder();
+        then.productOrderSearchedResponseIsValid(response);
     }
 
     @Test
-    @Order(2)
-    void shouldSucceededWhenCallUpdate() {
-
-        given()
-            .port(port)
-            .contentType(ContentType.JSON)
-            .pathParam("id", STATIC_UUID.toString())
-            .body("{\"description\": \"update-test-01\",\"items\": [{\"productId\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\",\"quantity\": 5}]}")
-            .log().all()
-            .when()
-            .put(STATIC_URI + "{id}")
-            .then()
-            .assertThat()
-            .statusCode(HttpStatus.ACCEPTED.value());
-
+    void shouldSucceededWhenCallUpdate() throws Exception {
+        String productOrderUpdateRequestJSON = given.productOrderUpdateRequestJSON();
+        Response response = when.updateProductOrder(productOrderUpdateRequestJSON);
+        then.productOrderUpdatedResponseIsValid(response);
     }
 
     @Test
-    @Order(3)
     void shouldSucceededWhenCallDelete() {
-
-        given()
-            .port(port)
-            .contentType(ContentType.JSON)
-            .pathParam("id", STATIC_UUID.toString())
-            .log().all()
-            .when()
-            .delete(STATIC_URI + "{id}")
-            .then()
-            .assertThat()
-            .statusCode(HttpStatus.NO_CONTENT.value());
-
+        Response response = when.deleteProductOrtder();
+        then.productOrderDeletedResponseIsValid(response);
     }
 
     @Test
-    @Order(4)
     void shouldSucceededWhenCallListAll() {
-        when(service.listAll()).thenReturn(
-                Collections.singletonList(new ProductOrderRecordIn(
-                        STATIC_UUID,
-                        "product_order_test",
-                        "A",
-                        Collections.emptySet())));
-
-        given()
-                .port(port)
-                .contentType(ContentType.JSON)
-                .log().all()
-                .when()
-                .get(STATIC_URI)
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.OK.value());
-
+        given.productOrderListed();
+        Response response = when.findAllProductOrder();
+        then.productOrderFindAllResponseIsValid(response);
     }
 
-   
+
+    private class Given {
+        public void productOrderCreationService() {
+            doReturn(productorderRecordIn)
+                    .when(service)
+                        .create(any(ProductOrderRecordIn.class));
+        }
+
+        public String productOrderRequestJSON() throws JsonProcessingException {
+            ProductOrderRequestDTO request = ProductOrderRequestDTO.builder()
+                    .description("test-01")
+                    .items(Collections.singletonList(new OrderItemDTO(UUID.randomUUID(), 1)))
+                    .build();
+
+            return new ObjectMapper().writeValueAsString(request);
+        }
+
+        public void productOrderSearched() {
+            doReturn(productorderRecordIn)
+                    .when(service)
+                            .findById(any(UUID.class));
+        }
+
+        public String productOrderUpdateRequestJSON() throws JsonProcessingException {
+            ProductOrderRequestDTO request = ProductOrderRequestDTO.builder()
+                    .description("test-01")
+                    .items(Collections.singletonList(new OrderItemDTO(UUID.randomUUID(), 1)))
+                    .build();
+
+            return new ObjectMapper().writeValueAsString(request);
+        }
+
+        public void productOrderListed() {
+            List<ProductOrderRecordIn> retVal = Collections.singletonList(new ProductOrderRecordIn(
+                    PRODUCT_ORDER_UUID,
+                    "product_order_test",
+                    "A",
+                    Collections.singleton(new OrderItemRecord(UUID.randomUUID(), 1))));
+
+            doReturn(retVal)
+                    .when(service)
+                            .listAll();
+        }
+    }
+
+    private class When {
+        public Response postProductOrder(String productOrderRequestJSON) {
+            return given()
+                    .port(port)
+                    .contentType(ContentType.JSON)
+                    .body(productOrderRequestJSON)
+                    .log().all()
+                    .when()
+                    .post(STATIC_URI)
+                    .then()
+                    .assertThat()
+                    .statusCode(HttpStatus.CREATED.value())
+                    .extract()
+                    .response();
+        }
+
+        public Response findProductOrder() {
+            return given()
+                    .port(port)
+                    .contentType(ContentType.JSON)
+                    .pathParam("id", PRODUCT_ORDER_UUID.toString())
+                    .when()
+                    .get(STATIC_URI + "{id}")
+                    .then()
+                    .log().all()
+                    .assertThat()
+                    .statusCode(HttpStatus.OK.value())
+                    .extract()
+                    .response();
+        }
+
+        public Response updateProductOrder(String productOrderUpdateRequestJSON) {
+            return given()
+                    .port(port)
+                    .contentType(ContentType.JSON)
+                    .pathParam("id", PRODUCT_ORDER_UUID.toString())
+                    .body(productOrderUpdateRequestJSON)
+                    .log().all()
+                    .when()
+                    .put(STATIC_URI + "{id}");
+        }
+
+        public Response deleteProductOrtder() {
+            return given()
+                    .port(port)
+                    .contentType(ContentType.JSON)
+                    .pathParam("id", PRODUCT_ORDER_UUID.toString())
+                    .log().all()
+                    .when()
+                    .delete(STATIC_URI + "{id}");
+        }
+
+        public Response findAllProductOrder() {
+            return given()
+                    .port(port)
+                    .contentType(ContentType.JSON)
+                    .log().all()
+                    .when()
+                    .get(STATIC_URI);
+        }
+    }
+
+    private class Then {
+        public void productOrderCreationResponseIsValid(Response response) {
+            assertNotNull(response);
+            String idProductOrder = response.getHeader("Location");
+            idProductOrder = idProductOrder.substring(idProductOrder.lastIndexOf("/")+1);
+            assertNotNull(idProductOrder);
+            assertFalse(idProductOrder.isEmpty());
+            PRODUCT_ORDER_UUID = UUID.fromString(idProductOrder);
+        }
+
+        public void productOrderSearchedResponseIsValid(Response response) {
+            assertNotNull(response);
+            assertNotNull(response.getBody().jsonPath().getString("id"));
+        }
+
+        public void productOrderUpdatedResponseIsValid(Response response) {
+            assertNotNull(response);
+            assertEquals(response.getStatusCode(), HttpStatus.ACCEPTED.value());
+        }
+
+        public void productOrderDeletedResponseIsValid(Response response) {
+            assertNotNull(response);
+            assertEquals(response.getStatusCode(), HttpStatus.NO_CONTENT.value());
+        }
+
+        public void productOrderFindAllResponseIsValid(Response response) {
+            assertNotNull(response);
+            assertEquals(response.getStatusCode(), HttpStatus.OK.value());
+            assertInstanceOf(ArrayList.class, response.getBody().jsonPath().get());
+        }
+    }
 }
